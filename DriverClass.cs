162ef8d93c3ccf45bc3378928f09e36a8ade44d8
VitaliyRud2001 <53946268+VitaliyRud2001@users.noreply.github.com>
2019-11-10 09:23:51 -0500

    Create DriverClass.cs

---
+++ DriverClass.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace Test
+{
+    public class DriverClass
+    {
+        static public bool IfElemPresentInDriver(DriveInfo drive, DriveInfo[] drivesOnStart)
+        {
+            foreach (var driveOnStart in drivesOnStart)
+            {
+                if (drive.Name == driveOnStart.Name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static public DriveInfo GetLastAddedDriver(DriveInfo[] drivesAfterInput, DriveInfo[] drivesOnStart)
+        {
+            foreach (var driveAfterInput in drivesAfterInput)
+            {
+                if (IfElemPresentInDriver(driveAfterInput, drivesOnStart) == false)
+                {
+                    return driveAfterInput;
+                }
+
+            }
+            return null;
+        }
+    }
+}

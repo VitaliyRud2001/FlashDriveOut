f71dfbecb98368f125cdb8ec6fb601c5f0acae01
VitaliyRud2001 <53946268+VitaliyRud2001@users.noreply.github.com>
2019-11-10 09:22:27 -0500

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -14,15 +14,45 @@ namespace Test
         private static string[] Scopes = { DriveService.Scope.Drive };
         private static String ApplicationName = "FlashDriveConnect";
 
+
+
+
+
         public static void Main(string[] args)
         {
+            DriveInfo[] drivesOnStart = DriveInfo.GetDrives();
+            bool deviceIsInput = false;
+
             DriveService service = GoogleDriveConnect.GetDriveService();
             FlashDriveConnect flashDriveConnect = new FlashDriveConnect();
-            flashDriveConnect.SendFilesOnGoogleDrive("/home/vitaliyrud/Downloads/", service);
-            Console.WriteLine("done!");
+            while (true)
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                if (drives.Length != drivesOnStart.Length && deviceIsInput == false)
+                {
+                    deviceIsInput = true;
+                    DriveInfo last_driver = DriverClass.GetLastAddedDriver(drives,drivesOnStart);
+                    Console.WriteLine("Drive found! Name:{0}", last_driver!=null ? $"{last_driver.Name}":"Empty!");
+                    flashDriveConnect.SendFilesOnGoogleDrive(last_driver.Name, service);
+                    Console.WriteLine("Files sent!");
+                    drivesOnStart = drives;
+                }
+                if (drives.Length < drivesOnStart.Length && deviceIsInput == true)
+                {
+                    deviceIsInput = false;
+                    Console.WriteLine("Device output!");
+                    drivesOnStart = drives;
+                }
+                Thread.Sleep(3000);
+
+
+
+            }
+
         }
-       
-    
+
+
 
     }
 }
+
